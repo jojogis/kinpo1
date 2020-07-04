8b9a96a248233af333acb8861b24e7afdaed923c
jojogis <jojogis.lesha@yandex.ru>
2020-07-04 20:42:04 +0400

    Добавлен тест checkRuleToBe_twoPersAndCompProfs

--- Test/tst_maintests.cpp
+++ Test/tst_maintests.cpp
@@ -42,6 +42,7 @@ private slots:
     void checkRuleToBe_notInList();
     void checkRuleToBe_twoCharactersAndManyProf();
     void checkRuleToBe_compProf();
+    void checkRuleToBe_twoPersAndCompProfs();
     void checkRuleAppos1_noProf();
     void checkRuleAppos1_oneProf();
     void checkRuleAppos1_compProf();
@@ -611,6 +612,28 @@ void MainTests::checkRuleToBe_compProf(){
     sentence.tokens.append(Token( "a" , "a" , 3 , "DT" , "O" ));
     sentence.tokens.append(Token( "police" , "police" , 4 , "NN" , "TITLE" ));
     sentence.tokens.append(Token( "officer" , "officer" , 5 , "NN" , "TITLE" ));
+    sentence.getById( 5 ).setDep( "nsubj" , 1 );
+    sentence.getById( 5 ).setDep( "compound" , 4 );
+    Rules::checkRuleToBe(profNames,sentence,profList);
+    QMultiHash<QString,QString> expected;
+    expected.insert("Mike","police officer");
+    QString message;
+    QVERIFY2(compareMultiHash(profNames,expected,message),message.toUtf8());
+
+}
+
+void MainTests::checkRuleToBe_twoPersAndCompProfs(){
+    QMultiHash<QString,QString> profNames;
+    Sentence sentence;
+    QStringList profList;
+    profList.append(perpList);
+    sentence.tokens.append(Token( "Mike" , "Mike" , 1 , "NNP" , "PERSON" ));
+    sentence.tokens.append(Token( "is" , "be" , 2 , "VBZ" , "O" ));
+    sentence.tokens.append(Token( "a" , "a" , 3 , "DT" , "O" ));
+    sentence.tokens.append(Token( "police" , "police" , 4 , "NN" , "TITLE" ));
+    sentence.tokens.append(Token( "officer" , "officer" , 5 , "NN" , "TITLE" ));
+    sentence.tokens.append(Token( "and" , "and" , 6 , "CC" , "O" ));
+    sentence.tokens.append(Token( "doctor" , "doctor" , 7 , "NN" , "TITLE" ));
     sentence.getById( 5 ).setDep( "nsubj" , 1 );
     sentence.getById( 5 ).setDep( "compound" , 4 );
     Rules::checkRuleToBe(profNames,sentence,profList);
